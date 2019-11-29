35c502105552dd07fd504460a383ffd0ee4ba549
Rodolfo <rod_dolpho@hotmail.com>
2019-11-29 19:56:25 -0300

    ADD New Pages, ADD Dashboard Page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import React from 'react';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+
+import SelectProvider from '~/pages/New/SelectProvider';
+import Confirm from '~/pages/New/Confirm';
+import SelectDateTime from '~/pages/New/SelectDateTime';
 
 export default (signedIn = false) =>
   createAppContainer(
@@ -13,9 +22,40 @@ export default (signedIn = false) =>
           SignIn,
           SignUp,
         }),
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            New: {
+              screen: createStackNavigator({
+                SelectProvider,
+                SelectDateTime,
+                Confirm,
+              }),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255,255,255, 0.6)"
+                  />
+                ),
+              },
+            },
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255,255,255,0.7)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
       },
       {
         initialRouteName: signedIn ? 'App' : 'Sign',
